96e8a42dd3b44a8372c71c2bd99fdf2f087087cf
NICOLAS <nicolasabarc@gmail.com>
2017-01-11 00:36:54 -0200

    v.1.3

--- resources/js/nicolas.js
+++ resources/js/nicolas.js
@@ -1,13 +1,3 @@
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1000, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
-
 /**/
 
 $('body').scrollspy({
@@ -20,20 +10,59 @@ $('.navbar-collapse ul li a').click(function() {
 
 /*******/
 
+
+function giraElemento() {
+
+    var $cog = $('.fa-cog');
+
+    $cog.hover(function() {
+        $(this).toggleClass('fa-spin');
+    });
+
+}
+
+function escalaElemento() {
+
+    var $iconos = $(".fa-mobile,.fa-tablet,.fa-laptop");
+
+    $iconos.hover(function agranda() {
+
+        $(this).css('transform', 'scale(1.6)');
+
+    }, function normaliza() {
+
+        $(this).css('transform', 'scale(1)');
+
+    });
+
+}
+
+
+function desplazamientoMenu() {
+
+    var $pageScroll = $('a.page-scroll');
+    var $html_body = $('html, body');
+
+    $pageScroll.click(function(e) {
+        var $anchor = $(this);
+        $html_body.stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000, 'easeInOutExpo');
+        e.preventDefault();
+    });
+}
+
+
+function _render() {
+
+    giraElemento();
+    escalaElemento();
+    desplazamientoMenu();
+}
+
+
 $(function() {
 
-    $(".fa.fa-refresh").hover(function() {
-
-        $(this).toggleClass('fa-spin');
-
-    });
-
-    $("#head-title").typed({
-        strings: ["Nicolás Abarca ^1200", " < Diseño Web /> ^950", " { Programacíon } ^950", "Contáctame ! ^5000"],
-        typeSpeed: 50,
-        loop: true,
-        startDelay: 15
-    });
-
+    _render();
 
 });
