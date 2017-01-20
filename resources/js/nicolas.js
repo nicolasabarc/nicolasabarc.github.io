fa60cd7aafa2fb661b063ee04b7e6d81d6acaccc
NICOLAS <nicolasabarc@gmail.com>
2017-01-20 13:49:38 -0200

    v1.5.2

--- resources/js/nicolas.js
+++ resources/js/nicolas.js
@@ -70,11 +70,41 @@ function escribeHeader() {
 
     $contactmail.typed({
         strings: [" nicolasabarc @ gmail . com  ^3500"],
-        typeSpeed: 10,      
+        typeSpeed: 10,
         loop: true,
         startDelay: 0
     });
 
+}
+
+function revealOnScroll() {
+
+    // use rotation in reveal configuration
+    sr.reveal('.service', {
+        reset: true,
+        delay: 200,
+        distance: '90px',
+        easing: 'ease-in-out',
+        scale: 1.1,
+        mobile: true
+    });
+
+    sr.reveal('.techIcon', {
+        reset: true,
+        delay: 100,
+        distance: '200px',
+        easing: 'ease-in',
+        scale: 1.2,
+        mobile: true
+    });
+
+    sr.reveal('#mail', {
+        reset: false,
+        distance: '200px',
+        easing: 'ease-out',
+        scale: 1.2,
+        mobile: true
+    });
 }
 
 /*******-----> <------*******/
@@ -85,6 +115,7 @@ function _render() {
     escalaElemento();
     desplazamientoMenu();
     escribeHeader();
+    revealOnScroll();
 }
 
 /*******-----> <------*******/
